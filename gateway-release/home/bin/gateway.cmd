15b57d0cb1846926cb3dab803543113f3d0c5e96
Kevin Minder <kevin.minder@hortonworks.com>
2014-03-21 16:01:46 -0400

    KNOX-320: Simplify scripts for using Knox on windows (fix). (cherry picked from commit 83fe6647f3122378e1eae739df37c4d8041bc424)

--- gateway-release/home/bin/gateway.cmd
+++ gateway-release/home/bin/gateway.cmd
@@ -1,64 +1,35 @@
-@REM
-
-@REM Licensed to the Apache Software Foundation (ASF) under one or more
-
-@REM contributor license agreements. See the NOTICE file distributed with
-
-@REM this work for additional information regarding copyright ownership.
-
-@REM The ASF licenses this file to You under the Apache License, Version 2.0
-
-@REM (the "License"); you may not use this file except in compliance with
-
-@REM the License. You may obtain a copy of the License at
-
-@REM
-
-@REM http://www.apache.org/licenses/LICENSE-2.0
-
-@REM
-
-@REM Unless required by applicable law or agreed to in writing, software
-
-@REM distributed under the License is distributed on an "AS IS" BASIS,
-
-@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
-@REM See the License for the specific language governing permissions and
-
-@REM limitations under the License.
-
-@REM
-
-@ECHO OFF
-
-
+@echo off
+REM Licensed to the Apache Software Foundation (ASF) under one or more
+REM contributor license agreements. See the NOTICE file distributed with
+REM this work for additional information regarding copyright ownership.
+REM The ASF licenses this file to You under the Apache License, Version 2.0
+REM (the "License"); you may not use this file except in compliance with
+REM the License. You may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
 
 REM Start/stop script location
-
-SET APP_BIN_DIR=%~dp0
-
-
+set APP_BIN_DIR=%~dp0
 
 REM The app's jar name
-
-SET APP_JAR=%APP_BIN_DIR%gateway.jar
-
-
+set APP_JAR=%APP_BIN_DIR%gateway.jar
 
 if not exist "%JAVA_HOME%"\bin\java.exe (
-
   echo Error: JAVA_HOME is incorrectly set.
-
   exit /B 1
-
 )
 set JAVA="%JAVA_HOME%"\bin\java
 
+"%JAVA%" -jar "%APP_JAR%"
 
-
-"%JAVA%" -jar "%APP_JAR%
-
-"
+if not %ERRORLEVEL% == 0 (
+  exit /B %ERRORLEVEL%
+)
 
 exit /B 0
