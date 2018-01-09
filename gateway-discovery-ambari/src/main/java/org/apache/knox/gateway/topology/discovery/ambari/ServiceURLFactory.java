92e2ec59a5940a9e7c67ec5cd29044f811dee40a
Sandeep More <more@apache.org>
2018-01-09 14:51:08 -0500

    KNOX-998 - Merge from 0.14.0 master

--- gateway-discovery-ambari/src/main/java/org/apache/knox/gateway/topology/discovery/ambari/ServiceURLFactory.java
+++ gateway-discovery-ambari/src/main/java/org/apache/knox/gateway/topology/discovery/ambari/ServiceURLFactory.java
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.apache.hadoop.gateway.topology.discovery.ambari;
+package org.apache.knox.gateway.topology.discovery.ambari;
 
 import java.util.ArrayList;
 import java.util.HashMap;
