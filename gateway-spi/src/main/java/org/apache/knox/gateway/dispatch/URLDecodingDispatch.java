27c1c0f8d10e3a763fc7b676d5f9c4ed087fc870
Sandeep More <more@apache.org>
2018-05-21 11:44:16 -0400

    KNOX-1326 - Fix the Datanode link on the Namenode page

---
+++ gateway-spi/src/main/java/org/apache/knox/gateway/dispatch/URLDecodingDispatch.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.knox.gateway.dispatch;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.URLDecoder;
+
+/**
+ * Dispatch which decodes the outgoing URLs (to services).
+ * This is useful in cases where the url is picked up
+ * from the query parameter and is already encoded.
+ *
+ * @since 1.1.0
+ */
+public class URLDecodingDispatch extends DefaultDispatch {
+
+  public URLDecodingDispatch() {
+    super();
+  }
+
+  @Override
+  public URI getDispatchUrl(final HttpServletRequest request) {
+    String decoded;
+
+    try {
+      decoded = URLDecoder.decode(request.getRequestURL().toString(), "UTF-8" );
+    } catch (final Exception e) {
+      /* fall back in case of exception */
+      decoded = request.getRequestURL().toString();
+    }
+
+    final StringBuffer str = new StringBuffer(decoded);
+    final String query = request.getQueryString();
+    if ( query != null ) {
+      str.append('?');
+      str.append(query);
+    }
+    final URI url = URI.create(str.toString());
+    return url;
+  }
+}
---
+++ gateway-spi/src/test/java/org/apache/knox/gateway/dispatch/URLDecodingDispatchTest.java
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership.  The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.knox.gateway.dispatch;
+
+import org.apache.knox.test.TestUtils;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test NoURLEncodingDispatch dispatch.
+ *
+ * @since 1.1.0
+ */
+public class URLDecodingDispatchTest {
+
+  HttpServletRequest request;
+  Dispatch dispatch = new URLDecodingDispatch();
+
+  public URLDecodingDispatchTest() {
+    super();
+  }
+
+  /**
+   * Test whether the encoded url is decoded properly.
+   *
+   * @throws Exception
+   */
+  @Test(timeout = TestUtils.MEDIUM_TIMEOUT)
+  public void testGetDispatchUrl() throws Exception {
+
+    final String path = "https://localhost:8443/gateway/sandbox/datanode/datanode.html?host=http%3A%2F%2Flocalhost%3A9864";
+    URI uri;
+
+    request = EasyMock.createNiceMock(HttpServletRequest.class);
+    EasyMock.expect(request.getRequestURI()).andReturn(path).anyTimes();
+    EasyMock.expect(request.getRequestURL()).andReturn(new StringBuffer(path))
+        .anyTimes();
+    EasyMock.expect(request.getQueryString()).andReturn(null).anyTimes();
+    EasyMock.replay(request);
+    uri = dispatch.getDispatchUrl(request);
+    assertThat(uri.toASCIIString(),
+        is("https://localhost:8443/gateway/sandbox/datanode/datanode.html?host=http://localhost:9864"));
+  }
+
+}
