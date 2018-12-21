4e04a520801d0a586af8446e7204397349b15a66
Kevin Risden <krisden@apache.org>
2018-12-21 10:58:06 -0500

    KNOX-1707 - Gateway Admin UI should have the right version
    Signed-off-by: Kevin Risden <krisden@apache.org>

--- gateway-admin-ui/admin-ui/app/gateway-version.component.ts
+++ gateway-admin-ui/admin-ui/app/gateway-version.component.ts
@@ -16,34 +16,30 @@
  */
 import {Component, OnInit} from '@angular/core';
 import {GatewayVersion} from './gateway-version';
-import {GatewayVersionService} from "./gateway-version.service";
-
+import {GatewayVersionService} from './gateway-version.service';
 
 @Component({
     selector: 'gateway-version',
     template: `
         <div *ngIf="gatewayVersion">
-            <span class="small"><cite>Knox Gateway Version</cite> {{this.gatewayVersion.version}}</span>
+            <span class="small"><cite>Version</cite> {{this.gatewayVersion.version}}</span>
             <span class="small"><cite>Hash</cite> {{this.gatewayVersion.hash}}</span>
-</div>`,
+        </div>`,
     providers: [GatewayVersionService]
 })
 
 export class GatewayVersionComponent implements OnInit {
+    gatewayVersion: GatewayVersion;
 
-    gatewayVersion : GatewayVersion;
-
-    constructor(private gatewayVersionService : GatewayVersionService) {
+    constructor(private gatewayVersionService: GatewayVersionService) {
     }
 
     getVersion(): void {
-        this.gatewayVersionService.getVersion().then( gatewayVersion => this.gatewayVersion = gatewayVersion);
+        this.gatewayVersionService.getVersion().then(gatewayVersion => this.gatewayVersion = gatewayVersion);
     }
 
     ngOnInit(): void {
         this.getVersion();
     }
-
-
 }
 
