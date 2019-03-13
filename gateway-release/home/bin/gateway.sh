7d97dbeacf0f992670242d56303716a35d7f652d
Sandor Molnar <smolnar@apache.org>
2019-03-13 19:42:27 +0100

    KNOX-1804 - Moving copy-pasted bash functions to knox-function.sh (#71)

--- gateway-release/home/bin/gateway.sh
+++ gateway-release/home/bin/gateway.sh
@@ -35,9 +35,6 @@ APP_BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # The app's jar name
 APP_JAR="$APP_BIN_DIR/gateway.jar"
 
-# The app's home dir
-APP_HOME_DIR=`dirname $APP_BIN_DIR`
-
 # The app's conf dir
 DEFAULT_APP_CONF_DIR="$APP_HOME_DIR/conf"
 APP_CONF_DIR=${KNOX_GATEWAY_CONF_DIR:-$DEFAULT_APP_CONF_DIR}
@@ -59,9 +56,6 @@ APP_MEM_OPTS="$KNOX_GATEWAY_MEM_OPTS"
 # The app's debugging options
 APP_DBG_OPTS="$KNOX_GATEWAY_DBG_OPTS"
 
-# The app's PID
-APP_PID=0
-
 #dynamic library path
 DEFAULT_JAVA_LIB_PATH="-Djava.library.path=$APP_HOME_DIR/ext/native"
 APP_JAVA_LIB_PATH=${KNOX_GATEWAY_JAVA_LIB_PATH:-$DEFAULT_JAVA_LIB_PATH}
@@ -75,11 +69,10 @@ APP_PID_FILE="$APP_PID_DIR/$APP_NAME.pid"
 APP_OUT_FILE="$APP_LOG_DIR/$APP_NAME.out"
 APP_ERR_FILE="$APP_LOG_DIR/$APP_NAME.err"
 
-# The start wait time
-APP_START_WAIT_TIME=2
+DEFAULT_APP_RUNNING_IN_FOREGROUND="$GATEWAY_SERVER_RUN_IN_FOREGROUND"
+APP_RUNNING_IN_FOREGROUND=${KNOX_GATEWAY_RUNNING_IN_FOREGROUND:-DEFAULT_APP_RUNNING_IN_FOREGROUND}
 
-# The kill wait time limit
-APP_KILL_WAIT_TIME=10
+APP_JAVA_OPTS="$APP_JAVA_LIB_PATH $APP_MEM_OPTS $APP_DBG_OPTS $APP_LOG_OPTS"
 
 function main {
    checkJava
@@ -92,6 +85,7 @@ function main {
          if [ "$2" = "--printEnv" ]; then
            printEnv
          fi
+         checkEnv
          appStart
          ;;
       stop)   
@@ -116,149 +110,6 @@ function setupEnv {
    checkEnv
    $JAVA -jar $APP_JAR -persist-master -nostart
    return 0
-}
-
-function appStart {
-   checkEnv
-
-   if [ "$GATEWAY_SERVER_RUN_IN_FOREGROUND" == true ]; then
-      $JAVA $APP_JAVA_LIB_PATH $APP_MEM_OPTS $APP_DBG_OPTS $APP_LOG_OPTS -jar $APP_JAR
-   else
-      getPID
-      if [ "$?" -eq "0" ]; then
-         printf "$APP_LABEL is already running with PID $APP_PID.\n"
-         exit 0
-      fi
-  
-      printf "Starting $APP_LABEL "
-   
-      rm -f $APP_PID_FILE
-
-      nohup $JAVA $APP_JAVA_LIB_PATH $APP_MEM_OPTS $APP_DBG_OPTS $APP_LOG_OPTS -jar $APP_JAR >>$APP_OUT_FILE 2>>$APP_ERR_FILE & printf $!>$APP_PID_FILE || exit 1
-
-      getPID
-      for ((i=0; i<APP_START_WAIT_TIME*10; i++)); do
-         appIsRunning $APP_PID
-         if [ "$?" -eq "0" ]; then break; fi
-         sleep 0.1
-      done
-      appIsRunning $APP_PID
-      if [ "$?" -ne "1" ]; then
-         printf "failed.\n"
-         rm -f $APP_PID_FILE
-         exit 1
-      fi
-      printf "succeeded with PID $APP_PID.\n"
-      return 0
-   fi
-}
-
-function appStop {
-   getPID
-   appIsRunning $APP_PID
-   if [ "$?" -eq "0" ]; then
-     printf "$APP_LABEL is not running.\n"
-     rm -f $APP_PID_FILE
-     return 0
-   fi
-  
-   printf "Stopping $APP_LABEL with PID $APP_PID "
-   appKill $APP_PID >>$APP_OUT_FILE 2>>$APP_ERR_FILE
-
-   if [ "$?" -ne "0" ]; then
-     printf "failed. \n"
-     exit 1
-   else
-     rm -f $APP_PID_FILE
-     printf "succeeded.\n"
-     return 0
-   fi
-}
-
-function appStatus {
-   printf "$APP_LABEL "
-   getPID
-   if [ "$?" -eq "1" ]; then
-     printf "is not running. No PID file found.\n"
-     return 0
-   fi
-
-   appIsRunning $APP_PID
-   if [ "$?" -eq "1" ]; then
-     printf "is running with PID $APP_PID.\n"
-     exit 1
-   else
-     printf "is not running.\n"
-     return 0
-   fi
-}
-
-# Removed the app PID file if app is not run
-function appClean {
-   getPID
-   appIsRunning $APP_PID
-   if [ "$?" -eq "0" ]; then
-     deleteLogFiles
-     return 0
-   else
-     printf "Can't clean files.  $APP_LABEL is running with PID $APP_PID.\n"
-     exit 1
-   fi
-}
-
-function appKill {
-   local localPID=$1
-   kill $localPID || return 1
-   for ((i=0; i<APP_KILL_WAIT_TIME*10; i++)); do
-      appIsRunning $localPID
-      if [ "$?" -eq "0" ]; then return 0; fi
-      sleep 0.1
-   done
-
-   kill -s KILL $localPID || return 1
-   for ((i=0; i<APP_KILL_WAIT_TIME*10; i++)); do
-      appIsRunning $localPID
-      if [ "$?" -eq "0" ]; then return 0; fi
-      sleep 0.1
-   done
-
-   return 1
-}
-
-# Returns 0 if the app is running and sets the $PID variable.
-function getPID {
-   if [ ! -d $APP_PID_DIR ]; then
-      printf "Can't find PID dir.\n"
-      exit 1
-   fi
-   if [ ! -f $APP_PID_FILE ]; then
-     APP_PID=0
-     return 1
-   fi
-   
-   APP_PID="$(<$APP_PID_FILE)"
-
-   ps -p $APP_PID > /dev/null
-   # if the exit code was 1 then it isn't running
-   # and it is safe to start
-   if [ "$?" -eq "1" ];
-   then
-     return 1
-   fi
-
-   return 0
-}
-
-function appIsRunning {
-   if [ "$1" -eq "0" ]; then return 0; fi
-
-   ps -p $1 > /dev/null
-
-   if [ "$?" -eq "1" ]; then
-     return 0
-   else
-     return 1
-   fi
 }
 
 function checkReadDir {
@@ -297,17 +148,6 @@ function checkEnv {
 
     checkWriteDir $APP_LOG_DIR
     checkWriteDir $APP_PID_DIR
-}
-
-function deleteLogFiles {
-     rm -f $APP_PID_FILE
-     printf "Removed the $APP_LABEL PID file: $APP_PID_FILE.\n"
-     
-     rm -f $APP_OUT_FILE
-     printf "Removed the $APP_LABEL OUT file: $APP_OUT_FILE.\n"
-     
-     rm -f $APP_ERR_FILE
-     printf "Removed the $APP_LABEL ERR file: $APP_ERR_FILE.\n"
 }
 
 function printHelp {
