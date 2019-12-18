02abf8f2a533abfd9234e5dfd849f858a9ae2050
nxverma <nxverma@gmail.com>
2019-12-17 17:14:18 -0800

    KNOX-2149-As per review merger code in JWTProvider . removed JWKS provider

--- gateway-provider-security-jwt/src/main/java/org/apache/knox/gateway/provider/federation/jwt/filter/JWTFederationFilter.java
+++ gateway-provider-security-jwt/src/main/java/org/apache/knox/gateway/provider/federation/jwt/filter/JWTFederationFilter.java
@@ -38,7 +38,10 @@ public class JWTFederationFilter extends AbstractJWTFilter {
   public static final String KNOX_TOKEN_AUDIENCES = "knox.token.audiences";
   public static final String TOKEN_VERIFICATION_PEM = "knox.token.verification.pem";
   private static final String KNOX_TOKEN_QUERY_PARAM_NAME = "knox.token.query.param.name";
+  public static final String TOKEN_PRINCIPAL_CLAIM = "knox.token.principal.claim";
+  public static final String JWKS_URL = "knox.token.jwks.url";
   private static final String BEARER = "Bearer ";
+  private String HEADER_TOKEN_NAME = "accesstoken";
   private String paramName = "knoxtoken";
 
   @Override
@@ -56,7 +59,16 @@ public class JWTFederationFilter extends AbstractJWTFilter {
     if (queryParamName != null) {
       paramName = queryParamName;
     }
-
+    //  JWKSUrl
+    String oidcjwksurl = filterConfig.getInitParameter(JWKS_URL);
+    if (oidcjwksurl != null) {
+      expectedJWKSUrl = oidcjwksurl;
+    }
+    // expected claim
+    String oidcPrincipalclaim = filterConfig.getInitParameter(TOKEN_PRINCIPAL_CLAIM);
+    if (oidcPrincipalclaim != null) {
+      expectedPrincipalClaim = oidcPrincipalclaim;
+    }
     // token verification pem
     String verificationPEM = filterConfig.getInitParameter(TOKEN_VERIFICATION_PEM);
     // setup the public key of the token issuer for verification
@@ -79,6 +91,10 @@ public class JWTFederationFilter extends AbstractJWTFilter {
     if (header != null && header.startsWith(BEARER)) {
       // what follows the bearer designator should be the JWT token being used to request or as an access token
       wireToken = header.substring(BEARER.length());
+    }
+    else if((((HttpServletRequest) request).getHeader(HEADER_TOKEN_NAME))!= null) {
+     // to support JWT in Authorization Header without as BEARER in case of Hive JDBC and others
+      wireToken = ((HttpServletRequest) request).getHeader(HEADER_TOKEN_NAME);
     }
     else {
       // check for query param
