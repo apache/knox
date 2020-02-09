a34a92d139f3a8a237138dafa587ea03f915583a
Phil Zampino <pzampino@apache.org>
2020-02-08 23:11:35 -0500

    KNOX-2228 - JWTFilter should handle unknown token exception from token state service

--- gateway-service-knoxtoken/src/main/java/org/apache/knox/gateway/service/knoxtoken/TokenServiceMessages.java
+++ gateway-service-knoxtoken/src/main/java/org/apache/knox/gateway/service/knoxtoken/TokenServiceMessages.java
@@ -24,46 +24,12 @@ import org.apache.knox.gateway.i18n.messages.StackTrace;
 
 @Messages(logger="org.apache.knox.gateway.service.knoxtoken")
 public interface TokenServiceMessages {
-  @Message( level = MessageLevel.INFO, text = "About to redirect to original URL: {0}")
-  void aboutToRedirectToOriginal(String original);
-
-  @Message( level = MessageLevel.DEBUG, text = "Adding the following JWT token as a cookie: {0}")
-  void addingJWTCookie(String token);
-
-  @Message( level = MessageLevel.INFO, text = "Unable to find cookie with name: {0}")
-  void cookieNotFound(String name);
-
-  @Message( level = MessageLevel.ERROR, text = "Unable to properly send needed HTTP status code: {0}, {1}")
-  void unableToCloseOutputStream(String message, String string);
-
-  @Message( level = MessageLevel.ERROR, text = "Unable to add cookie to response. {0}: {1}")
-  void unableAddCookieToResponse(String message, String stackTrace);
-
-  @Message( level = MessageLevel.ERROR, text = "Original URL not found in request.")
-  void originalURLNotFound();
-
-  @Message( level = MessageLevel.INFO, text = "JWT cookie successfully added.")
-  void addedJWTCookie();
 
   @Message( level = MessageLevel.ERROR, text = "Unable to issue token.")
   void unableToIssueToken(@StackTrace( level = MessageLevel.DEBUG) Exception e);
 
-  @Message( level = MessageLevel.WARN,
-            text = "The SSO cookie SecureOnly flag is set to FALSE and is therefore insecure.")
-  void cookieSecureOnly(boolean secureOnly);
-
-  @Message( level = MessageLevel.WARN, text = "The SSO cookie max age configuration is invalid: {0} - using default.")
-  void invalidMaxAgeEncountered(String age);
-
   @Message( level = MessageLevel.WARN, text = "The SSO token time to live - ttl is invalid: {0} - using default.")
   void invalidTokenTTLEncountered(String ttl);
-
-  @Message( level = MessageLevel.INFO, text = "The cookie max age is being set to: {0}.")
-  void setMaxAge(String age);
-
-  @Message( level = MessageLevel.ERROR, text = "The original URL: {0} for redirecting back after authentication is " +
-      "not valid according to the configured whitelist: {1}. See documentation for KnoxSSO Whitelisting.")
-  void whiteListMatchFail(String original, String whitelist);
 
   @Message( level = MessageLevel.WARN,
             text = "Unable to acquire cert for endpoint clients - assume trust will be provisioned separately: {0}.")
