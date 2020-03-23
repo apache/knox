3d17ede66d29ccf41e95c469e56c696f99262df5
Phil Zampino <pzampino@apache.org>
2020-03-23 13:05:13 -0400

    KNOX-2301 and KNOX-2302 (#297)
    * KNOX-2301 - Trigger discovery for descriptors at gateway start time
    
    * KNOX-2302 - Skip re-deployment of generated topology if the topology has not changed

--- gateway-spi/src/main/java/org/apache/knox/gateway/topology/Topology.java
+++ gateway-spi/src/main/java/org/apache/knox/gateway/topology/Topology.java
@@ -19,13 +19,17 @@ package org.apache.knox.gateway.topology;
 
 import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.collections.map.MultiKeyMap;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Topology {
 
@@ -38,8 +42,12 @@ public class Topology {
   private Map<String,Map<String,Provider>> providerMap = new HashMap<>();
   public List<Service> services = new ArrayList<>();
   private MultiKeyMap serviceMap;
-  private List<Application> applications = new ArrayList<>();
+  List<Application> applications = new ArrayList<>();
   private Map<String,Application> applicationMap = new HashMap<>();
+
+  private ProviderComparator providerComparator = new ProviderComparator();
+  private ServiceComparator serviceComparator = new ServiceComparator();
+  private ApplicationComparator appComparator = new ApplicationComparator();
 
   public Topology() {
     serviceMap = MultiKeyMap.decorate(new HashedMap());
@@ -151,6 +159,108 @@ public class Topology {
     String role = provider.getRole();
     Map<String, Provider> nameMap = providerMap.computeIfAbsent(role, k -> new HashMap<>());
     nameMap.put( provider.getName(), provider );
+  }
+
+  @Override
+  public int hashCode() {
+    return (new HashCodeBuilder(17, 31)).append(name)
+                                .append(providerList.stream().sorted(providerComparator).collect(Collectors.toList()))
+                                .append(services.stream().sorted(serviceComparator).collect(Collectors.toList()))
+                                .append(applications.stream().sorted(appComparator).collect(Collectors.toList()))
+                                .build();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+
+    // Has to be a Topology
+    if (!(obj instanceof Topology)) {
+      return false;
+    }
+
+    Topology other = (Topology) obj;
+    if (Objects.equals(this.name,other.name)) {
+      // Order is NOT significant for providers, services, and applications
+
+      if (equalProviders(other)) {  // Providers
+        if (equalServices(other)) { // Services
+          if (equalApplications(other)) { // Applications
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private boolean equalProviders(Topology other) {
+    if (providerList != null) {
+      if (other.providerList == null) {
+        return false;
+      }
+
+      List<Provider> mySorted = providerList.stream().sorted(providerComparator).collect(Collectors.toList());
+      List<Provider> otherSorted = other.providerList.stream().sorted(providerComparator).collect(Collectors.toList());
+      return mySorted.equals(otherSorted);
+    } else {
+      return (other.providerList == null);
+    }
+  }
+
+  private boolean equalServices(Topology other) {
+    if (services != null) {
+      if (other.services == null) {
+        return false;
+      }
+
+      List<Service> mySorted = services.stream().sorted(serviceComparator).collect(Collectors.toList());
+      List<Service> otherSorted = other.services.stream().sorted(serviceComparator).collect(Collectors.toList());
+      return mySorted.equals(otherSorted);
+    } else {
+      return (other.services == null);
+    }
+  }
+
+  private boolean equalApplications(Topology other) {
+    if (applications != null) {
+      if (other.applications == null) {
+        return false;
+      }
+
+      List<Application> mySorted = applications.stream().sorted(appComparator).collect(Collectors.toList());
+      List<Application> otherSorted = other.applications.stream().sorted(appComparator).collect(Collectors.toList());
+      return mySorted.equals(otherSorted);
+    } else {
+      return (other.applications == null);
+    }
+  }
+
+  private static final class ProviderComparator implements Comparator<Provider> {
+    @Override
+    public int compare(Provider o1, Provider o2) {
+      String name1 = o1.getName();
+      return name1 != null ? name1.compareToIgnoreCase(o2.getName()) : 0;
+    }
+  }
+
+  private static final class ServiceComparator implements Comparator<Service> {
+    @Override
+    public int compare(Service o1, Service o2) {
+      String name1 = o1.getName();
+      return name1 != null ? name1.compareToIgnoreCase(o2.getName()) : 0;
+    }
+  }
+
+  private static final class ApplicationComparator implements Comparator<Application> {
+    @Override
+    public int compare(Application o1, Application o2) {
+      String name1 = o1.getName();
+      return name1 != null ? name1.compareToIgnoreCase(o2.getName()) : 0;
+    }
   }
 
 }
