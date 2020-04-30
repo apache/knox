fb64225abd60e14420a66857048326ccc20e299c
YYTVicky <61596169+YYTVicky@users.noreply.github.com>
2020-04-30 00:19:45 -0400

    Update ConfigurableEncryptor.java

--- gateway-spi/src/main/java/org/apache/knox/gateway/services/security/impl/ConfigurableEncryptor.java
+++ gateway-spi/src/main/java/org/apache/knox/gateway/services/security/impl/ConfigurableEncryptor.java
@@ -107,11 +107,11 @@ public class ConfigurableEncryptor {
     rnd.nextBytes(salt);
 
     SecretKey tmp = getKeyFromPassword(new String(passPhrase), salt);
-    /* 
-    * the key should not be constant value, recommend code: 
-    * SecureRandom random = new SecureRandom(); 
+    /*
+    * the key should not be constant value, recommend code:
+    * SecureRandom random = new SecureRandom();
     * String defaultKey = String.valueOf(random.nextInt());
-    * byte[] keyBytes = defaultKey.getBytes(); 
+    * byte[] keyBytes = defaultKey.getBytes();
     * keyBytes = Arrays.copyOf(keyBytes,24);
     * SecretKey secret = new SecretKeySpec(keyBytes, alg);
     */
