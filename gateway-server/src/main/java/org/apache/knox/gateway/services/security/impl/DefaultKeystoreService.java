354c500842b81c02b2ad4dd275c76f736e5cef59
James Chen <jamchen@microsoft.com>
2020-11-15 03:46:23 -0800

    Copied and pasted the wrong lines

--- gateway-server/src/main/java/org/apache/knox/gateway/services/security/impl/DefaultKeystoreService.java
+++ gateway-server/src/main/java/org/apache/knox/gateway/services/security/impl/DefaultKeystoreService.java
@@ -513,11 +513,11 @@ public class DefaultKeystoreService implements KeystoreService {
   // Package private for unit test access
   // We need this to be synchronized to prevent multiple threads from using at once
   synchronized KeyStore createKeyStore(Path keystoreFilePath, String keystoreType, char[] password) throws KeystoreServiceException {
-    if (Files.notExists(keystoreFilePath)) {
     // Ensure the parent directory exists...
     // This is symlink safe.
     Path parentPath = keystoreFilePath.getParent();
     if (!Files.isDirectory(parentPath)) {
+      try{
         // This will attempt to create all missing directories.  No failures will occur if the
         // directories already exist.
         Files.createDirectories(parentPath);
