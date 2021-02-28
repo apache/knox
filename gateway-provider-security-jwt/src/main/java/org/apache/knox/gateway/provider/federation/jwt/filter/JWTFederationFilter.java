b395f0ca183bc8fe2b3da6717b3c9428d9f8f4d0
lmccay <lmccay@apache.org>
2021-02-28 17:54:14 -0500

    KNOX-2539 - Enhance JWTProvider to accept token via HTTP Basic (#404)
    * KNOX-2539 - Enhance JWTProvider to accept token via HTTP Basic

--- gateway-provider-security-jwt/src/main/java/org/apache/knox/gateway/provider/federation/jwt/filter/JWTFederationFilter.java
+++ gateway-provider-security-jwt/src/main/java/org/apache/knox/gateway/provider/federation/jwt/filter/JWTFederationFilter.java
@@ -31,17 +31,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
+import java.util.Base64;
+import java.util.Locale;
 
 public class JWTFederationFilter extends AbstractJWTFilter {
 
-  public static final String KNOX_TOKEN_AUDIENCES = "knox.token.audiences";
-  public static final String TOKEN_VERIFICATION_PEM = "knox.token.verification.pem";
-  public static final String KNOX_TOKEN_QUERY_PARAM_NAME = "knox.token.query.param.name";
-  public static final String TOKEN_PRINCIPAL_CLAIM = "knox.token.principal.claim";
-  public static final String JWKS_URL = "knox.token.jwks.url";
-  private static final String BEARER = "Bearer ";
-  private String paramName = "knoxtoken";
+    public static final String KNOX_TOKEN_AUDIENCES = "knox.token.audiences";
+    public static final String TOKEN_VERIFICATION_PEM = "knox.token.verification.pem";
+    public static final String KNOX_TOKEN_QUERY_PARAM_NAME = "knox.token.query.param.name";
+    public static final String TOKEN_PRINCIPAL_CLAIM = "knox.token.principal.claim";
+    public static final String JWKS_URL = "knox.token.jwks.url";
+    private static final String BEARER = "Bearer ";
+    private static final String BASIC = "Basic";
+    private static final String TOKEN = "Token";
+    private String paramName;
 
   @Override
   public void init( FilterConfig filterConfig ) throws ServletException {
@@ -58,16 +63,19 @@ public class JWTFederationFilter extends AbstractJWTFilter {
     if (queryParamName != null) {
       paramName = queryParamName;
     }
+
     //  JWKSUrl
     String oidcjwksurl = filterConfig.getInitParameter(JWKS_URL);
     if (oidcjwksurl != null) {
       expectedJWKSUrl = oidcjwksurl;
     }
+
     // expected claim
     String oidcPrincipalclaim = filterConfig.getInitParameter(TOKEN_PRINCIPAL_CLAIM);
     if (oidcPrincipalclaim != null) {
       expectedPrincipalClaim = oidcPrincipalclaim;
     }
+
     // token verification pem
     String verificationPEM = filterConfig.getInitParameter(TOKEN_VERIFICATION_PEM);
     // setup the public key of the token issuer for verification
@@ -104,15 +112,36 @@ public class JWTFederationFilter extends AbstractJWTFilter {
     }
   }
 
-  public String getWireToken(ServletRequest request) {
-    final String header = ((HttpServletRequest) request).getHeader("Authorization");
-    if (header != null && header.startsWith(BEARER)) {
-      // what follows the bearer designator should be the JWT token being used to request or as an access token
-      return header.substring(BEARER.length());
-    } else {
-      // check for query param
-      return request.getParameter(paramName);
-    }
+  public String getWireToken(final ServletRequest request) {
+      String token = null;
+      final String header = ((HttpServletRequest)request).getHeader("Authorization");
+      if (header != null) {
+          if (header.startsWith(BEARER)) {
+              // what follows the bearer designator should be the JWT token being used
+            // to request or as an access token
+              token = header.substring(BEARER.length());
+          }
+          else if (header.toLowerCase(Locale.ROOT).startsWith(BASIC.toLowerCase(Locale.ROOT))) {
+              // what follows the Basic designator should be the JWT token being used
+            // to request or as an access token
+              token = this.parseFromHTTPBasicCredentials(token, header);
+          }
+      }
+      if (token == null) {
+          token = request.getParameter(this.paramName);
+      }
+      return token;
+  }
+
+  private String parseFromHTTPBasicCredentials(String token, final String header) {
+      final String base64Credentials = header.substring(BASIC.length()).trim();
+      final byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+      final String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+      final String[] values = credentials.split(":", 2);
+      if (values[0].equalsIgnoreCase(TOKEN)) {
+          token = values[1];
+      }
+      return token;
   }
 
   @Override
