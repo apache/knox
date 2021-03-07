646f821b0875393122d3a834213810336b997843
newHE3DBer <xuxiangyang6666@163.com>
2021-03-07 21:16:15 +0800

    KNOX-2475: url creation failure caused by spaces in url

--- gateway-spi/src/test/java/org/apache/knox/gateway/dispatch/ConfigurableDispatchTest.java
+++ gateway-spi/src/test/java/org/apache/knox/gateway/dispatch/ConfigurableDispatchTest.java
@@ -130,6 +130,17 @@ public class ConfigurableDispatchTest {
     uri = dispatch.getDispatchUrl( request );
     assertThat( uri.toASCIIString(), is( "http://test-host:42/test%2Cpath" ) );
 
+    // encoding in query %20 is not removed
+    path = "http://test-host:42/test%2Cpath";
+    query = "service_config_version_note.matches(Updated%20Kerberos-related%20configurations";
+    request = EasyMock.createNiceMock( HttpServletRequest.class );
+    EasyMock.expect( request.getRequestURI() ).andReturn( path ).anyTimes();
+    EasyMock.expect( request.getRequestURL() ).andReturn( new StringBuffer( path ) ).anyTimes();
+    EasyMock.expect( request.getQueryString() ).andReturn( query ).anyTimes();
+    EasyMock.replay( request );
+    uri = dispatch.getDispatchUrl( request );
+    assertThat( uri.toASCIIString(), is( "http://test-host:42/test%2Cpath?service_config_version_note.matches(Updated%20Kerberos-related%20configurations" ) );
+
     // encoding in query string is removed
     path = "http://test-host:42/test%2Cpath";
     query = "test%26name=test%3Dvalue";
