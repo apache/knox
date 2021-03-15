4906682f2f0b316bb6d2ddc102959df5eedeeddd
Sandor Molnar <smolnar@cloudera.com>
2021-03-15 02:39:19 +0100

    KNOX-2552 - Including the generated Token ID in the JSON response for the KnoxToken service (#415)

--- gateway-service-knoxtoken/src/test/java/org/apache/knox/gateway/service/knoxtoken/TokenServiceResourceTest.java
+++ gateway-service-knoxtoken/src/test/java/org/apache/knox/gateway/service/knoxtoken/TokenServiceResourceTest.java
@@ -198,6 +198,8 @@ public class TokenServiceResourceTest {
     String expiry = getTagValue(retString, "expires_in");
     assertNotNull(expiry);
 
+    assertNotNull(getTagValue(retString, "token_id"));
+
     // Verify the token
     JWT parsedToken = new JWTToken(accessToken);
     assertEquals("alice", parsedToken.getSubject());
