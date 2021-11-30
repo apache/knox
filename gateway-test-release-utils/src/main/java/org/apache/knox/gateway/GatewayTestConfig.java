3a18b13aeb729e98807e21d0efaac54c5483193e
Sandor Molnar <smolnar@apache.org>
2021-11-30 16:37:30 +0100

    KNOX-2692 - Topology redeployment can be configured to require changes in topologies (#520)

--- gateway-test-release-utils/src/main/java/org/apache/knox/gateway/GatewayTestConfig.java
+++ gateway-test-release-utils/src/main/java/org/apache/knox/gateway/GatewayTestConfig.java
@@ -767,6 +767,11 @@ public class GatewayTestConfig extends Configuration implements GatewayConfig {
   }
 
   @Override
+  public boolean topologyRedeploymentRequiresChanges() {
+    return false;
+  }
+
+  @Override
   public List<String> getXForwardContextAppendServices() {
     return null;
   }
