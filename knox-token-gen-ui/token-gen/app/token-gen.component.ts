e6b2bb86944b5c43f8126a35cfbfa37c41a8b533
MrtnBalazs <martonbalazs1999@gmail.com>
2022-09-30 10:17:13 +0200

    KNOX-2811 - Added new token-gen module where initialized the base of the angular token-gen application.

---
+++ knox-token-gen-ui/token-gen/app/token-gen.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+  selector: 'app-token-gen',
+  templateUrl: './token-gen.component.html',
+  styleUrls: [],
+  providers: []
+})
+export class TokenGen implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
