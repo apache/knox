5a70e628a74de26cf4b2370443777cd58abe64bf
Sandor Molnar <smolnar@apache.org>
2023-01-10 16:24:59 +0100

    KNOX-2859 - Token Management UI improvements (#713)
    Replaced the old Angular2 Datatable with a more modern Material Table implementation and configured filtering, sorting and pagination on both tables on the Token Management UI.

--- knox-token-management-ui/token-management/app/token.management.component.ts
+++ knox-token-management-ui/token-management/app/token.management.component.ts
@@ -14,13 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {TokenManagementService} from './token.management.service';
 import {KnoxToken} from './knox.token';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
     selector: 'app-token-management',
     templateUrl: './token.management.component.html',
+    styleUrls: ['../assets/token-management-ui.css'],
     providers: [TokenManagementService]
 })
 
@@ -29,9 +33,18 @@ export class TokenManagementComponent implements OnInit {
     tokenGenerationPageURL = window.location.pathname.replace(new RegExp('token-management/.*'), 'token-generation/index.html');
 
     userName: string;
-    knoxTokens: KnoxToken[];
-    doAsKnoxTokens: KnoxToken[];
+    knoxTokens: MatTableDataSource<KnoxToken> = new MatTableDataSource();
+    doAsKnoxTokens: MatTableDataSource<KnoxToken> = new MatTableDataSource();
     impersonationEnabled: boolean;
+
+    displayedColumns = ['tokenId', 'issued', 'expires', 'comment', 'metadata', 'actions'];
+    @ViewChild('ownPaginator') paginator: MatPaginator;
+    @ViewChild('ownSort') sort: MatSort = new MatSort();
+
+    impersonationDisplayedColumns = ['impersonation.tokenId', 'impersonation.issued', 'impersonation.expires', 'impersonation.comment',
+                                     'impersonation.metadata', 'impersonation.user.name'];
+    @ViewChild('impersonationPaginator') impersonationPaginator: MatPaginator;
+    @ViewChild('impersonationSort') impersonationSort: MatSort = new MatSort();
 
     toggleBoolean(propertyName: string) {
         this[propertyName] = !this[propertyName];
@@ -42,6 +55,53 @@ export class TokenManagementComponent implements OnInit {
     }
 
     constructor(private tokenManagementService: TokenManagementService) {
+        let isMatch: (record: KnoxToken, filter: String, impersonated: boolean) => boolean = (record, filter, impersonated) => {
+          let normalizedFilter = filter.trim().toLocaleLowerCase();
+          let matchesTokenId = record.tokenId.toLocaleLowerCase().includes(normalizedFilter);
+          let matchesComment = record.metadata.comment && record.metadata.comment.toLocaleLowerCase().includes(normalizedFilter);
+          let matchesCustomMetadata = false;
+          if (record.metadata.customMetadataMap) {
+            for (let entry of Array.from(Object.entries(record.metadata.customMetadataMap))) {
+	          if (entry[0].toLocaleLowerCase().includes(normalizedFilter) || entry[1].toLocaleLowerCase().includes(normalizedFilter)) {
+                  matchesCustomMetadata = true;
+                  break;
+              }
+            }
+          } else {
+            matchesCustomMetadata = true; // nothing to match
+          }
+
+          let matchesImpersonatedUserName = false;  // doAs username should be checked only if impersonation is enabled
+          if (impersonated) {
+              matchesImpersonatedUserName = record.metadata.userName.toLocaleLowerCase().includes(normalizedFilter);
+          }
+
+          return matchesTokenId || matchesComment || matchesCustomMetadata || matchesImpersonatedUserName;
+        };
+
+        this.knoxTokens.filterPredicate = function (record, filter) {
+	      return isMatch(record, filter, false);
+        };
+
+        this.doAsKnoxTokens.filterPredicate = function (record, filter) {
+          return isMatch(record, filter, true);
+        };
+
+        this.knoxTokens.sortingDataAccessor = (item, property) => {
+           switch(property) {
+             case 'metadata.comment': return item.metadata.comment;
+             default: return item[property];
+           }
+        };
+
+        this.doAsKnoxTokens.sortingDataAccessor = (item, property) => {
+           let normalizedPropertyName = property.replace('impersonation.', '');
+           switch(normalizedPropertyName) {
+             case 'metadata.comment': return item.metadata.comment;
+             case 'metadata.username': return item.metadata.userName;
+             default: return item[normalizedPropertyName];
+           }
+        };
     }
 
     ngOnInit(): void {
@@ -57,13 +117,29 @@ export class TokenManagementComponent implements OnInit {
     }
 
     fetchAllKnoxTokens(): void {
-        this.fetchKnoxTokens(true);
         this.fetchKnoxTokens(false);
+        this.fetchKnoxTokens(true);
     }
 
     fetchKnoxTokens(impersonated: boolean): void {
         this.tokenManagementService.getKnoxTokens(this.userName, impersonated)
-            .then(tokens => impersonated ? this.doAsKnoxTokens = tokens : this.knoxTokens = tokens);
+            .then(tokens => this.populateTokens(impersonated, tokens));
+    }
+
+    populateTokens(impersonated: boolean, tokens: KnoxToken[]) {
+        if (impersonated) {
+            this.doAsKnoxTokens.data = tokens;
+            setTimeout(() => {
+                this.doAsKnoxTokens.paginator = this.impersonationPaginator;
+                this.doAsKnoxTokens.sort = this.impersonationSort;
+            });
+        } else {
+            this.knoxTokens.data = tokens;
+            setTimeout(() => {
+                this.knoxTokens.paginator = this.paginator;
+                this.knoxTokens.sort = this.sort;
+            });
+        }
     }
 
     disableToken(tokenId: string) {
@@ -90,6 +166,10 @@ export class TokenManagementComponent implements OnInit {
         return Date.now() > expiration;
     }
 
+    getExpirationColor(expiration: number): string {
+        return this.isTokenExpired(expiration) ? 'red' : 'green';
+    }
+
     isImpersonationEnabled(): boolean {
         return this.impersonationEnabled;
     }
@@ -100,6 +180,16 @@ export class TokenManagementComponent implements OnInit {
         mdMap = knoxToken.metadata.customMetadataMap;
       }
       return Array.from(Object.entries(mdMap));
+    }
+
+    applyFilter(impersonated: boolean, filterValue: string) {
+        filterValue = filterValue.trim(); // Remove whitespace
+        filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+        if (impersonated) {
+            this.doAsKnoxTokens.filter = filterValue;
+        } else {
+            this.knoxTokens.filter = filterValue;
+        }
     }
 
 }
