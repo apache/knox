5de0633dbe1197857bb1cdc3fbe04214e86a3401
zeroflag <amagyar@cloudera.com>
2023-07-14 11:40:32 +0200

    KNOX-2938 - jwks.json doesn't have double quotes which makes json invalid

--- gateway-service-knoxtoken/src/main/java/org/apache/knox/gateway/service/knoxtoken/JWKSResource.java
+++ gateway-service-knoxtoken/src/main/java/org/apache/knox/gateway/service/knoxtoken/JWKSResource.java
@@ -39,6 +39,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
