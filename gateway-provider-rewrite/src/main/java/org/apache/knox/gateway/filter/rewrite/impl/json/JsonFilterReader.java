bd3972d94b30a96d55035b93746e975bdd02d599
berylzsh <50791733+upczsh@users.noreply.github.com>
2024-03-04 20:20:44 +0800

    KNOX-2995 - Support json parsing NaN values (#828)
    ---------
    
    Co-authored-by: zhaoshuaihua <zhaoshuaihua@kingsoft.com>

--- gateway-provider-rewrite/src/main/java/org/apache/knox/gateway/filter/rewrite/impl/json/JsonFilterReader.java
+++ gateway-provider-rewrite/src/main/java/org/apache/knox/gateway/filter/rewrite/impl/json/JsonFilterReader.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -77,6 +78,11 @@ class JsonFilterReader extends Reader {
     bufferingLevel = null;
     bufferingConfig = null;
     this.config = config;
+    jsonParserConfigInit();
+  }
+
+  private void jsonParserConfigInit() {
+    parser.enable( JsonReadFeature.ALLOW_NON_NUMERIC_NUMBERS.mappedFeature() );
   }
 
   @Override
