7246aa9dde2002fcf79b7e9f708d272af556c8e3
Sandeep Moré <moresandeep@gmail.com>
2024-03-12 10:27:37 -0400

    KNOX-3014 - Fix a bug where unauthenticated path configured in shiro provider throw exception

--- gateway-provider-security-shiro/src/main/java/org/apache/knox/gateway/filter/ShiroSubjectIdentityAdapter.java
+++ gateway-provider-security-shiro/src/main/java/org/apache/knox/gateway/filter/ShiroSubjectIdentityAdapter.java
@@ -31,7 +31,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.knox.gateway.ShiroMessages;
 import org.apache.knox.gateway.audit.api.Action;
 import org.apache.knox.gateway.audit.api.ActionOutcome;
 import org.apache.knox.gateway.audit.api.AuditContext;
@@ -40,12 +42,14 @@ import org.apache.knox.gateway.audit.api.AuditServiceFactory;
 import org.apache.knox.gateway.audit.api.Auditor;
 import org.apache.knox.gateway.audit.api.ResourceType;
 import org.apache.knox.gateway.audit.log4j.audit.AuditConstants;
+import org.apache.knox.gateway.i18n.messages.MessagesFactory;
 import org.apache.knox.gateway.security.GroupPrincipal;
 import org.apache.knox.gateway.security.PrimaryPrincipal;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 
 public class ShiroSubjectIdentityAdapter implements Filter {
+  private static final ShiroMessages LOG = MessagesFactory.get(ShiroMessages.class);
   private static final String SUBJECT_USER_GROUPS = "subject.userGroups";
   private static AuditService auditService = AuditServiceFactory.getAuditService();
   private static Auditor auditor = auditService.getAuditor(
@@ -96,54 +100,71 @@ public class ShiroSubjectIdentityAdapter implements Filter {
       };
       Subject shiroSubject = SecurityUtils.getSubject();
 
+      /**
+       * For cases when we want anonymous authentication to urls in shiro.
+       * This is when do not want authentication for jwks endpoints using
+       * shiro.
+       */
       if (shiroSubject == null || shiroSubject.getPrincipal() == null) {
-        throw new IllegalStateException("Unable to determine authenticated user from Shiro, please check that your Knox Shiro configuration is correct");
-      }
+        LOG.unauthenticatedPathBypass(((HttpServletRequest) request).getRequestURI());
+        final String principal = "anonymous";
+        javax.security.auth.Subject subject = new javax.security.auth.Subject();
+        subject.getPrincipals().add(new PrimaryPrincipal(principal));
+        AuditContext context = auditService.getContext();
+        context.setUsername( principal );
+        auditService.attachContext(context);
+        javax.security.auth.Subject.doAs( subject, action );
+    } else {
 
-      final String principal = shiroSubject.getPrincipal().toString();
-      Set<Principal> principals = new HashSet<>();
-      Principal p = new PrimaryPrincipal(principal);
-      principals.add(p);
-      AuditContext context = auditService.getContext();
-      context.setUsername( principal );
-      auditService.attachContext(context);
-      String sourceUri = (String)request.getAttribute( AbstractGatewayFilter.SOURCE_REQUEST_CONTEXT_URL_ATTRIBUTE_NAME );
-      auditor.audit( Action.AUTHENTICATION , sourceUri, ResourceType.URI, ActionOutcome.SUCCESS );
+        final String principal = shiroSubject.getPrincipal().toString();
+        Set<Principal> principals = new HashSet<>();
+        Principal p = new PrimaryPrincipal(principal);
+        principals.add(p);
+        AuditContext context = auditService.getContext();
+        context.setUsername(principal);
+        auditService.attachContext(context);
+        String sourceUri = (String) request.getAttribute(AbstractGatewayFilter.SOURCE_REQUEST_CONTEXT_URL_ATTRIBUTE_NAME);
+        auditor.audit(Action.AUTHENTICATION, sourceUri, ResourceType.URI,
+            ActionOutcome.SUCCESS);
 
-      Set<String> userGroups;
-      // map ldap groups saved in session to Java Subject GroupPrincipal(s)
-      if (SecurityUtils.getSubject().getSession().getAttribute(SUBJECT_USER_GROUPS) != null) {
-        userGroups = (Set<String>)SecurityUtils.getSubject().getSession().getAttribute(SUBJECT_USER_GROUPS);
-      } else { // KnoxLdapRealm case
-        if(  shiroSubject.getPrincipal() instanceof String ) {
-           userGroups = new HashSet<>(shiroSubject.getPrincipals().asSet());
-           userGroups.remove(principal);
-        } else { // KnoxPamRealm case
-           Set<Principal> shiroPrincipals = new HashSet<>(shiroSubject.getPrincipals().asSet());
-           userGroups = new HashSet<>(); // Here we are creating a new UserGroup
-                                               // so we don't need to remove a Principal
-                                               // In the case of LDAP the userGroup may have already Principal
-                                               // added to the list of groups, so it is not needed.
-           for( Principal shiroPrincipal: shiroPrincipals ) {
-                userGroups.add(shiroPrincipal.toString() );
-           }
+        Set<String> userGroups;
+        // map ldap groups saved in session to Java Subject GroupPrincipal(s)
+        if (SecurityUtils.getSubject().getSession().getAttribute(SUBJECT_USER_GROUPS) != null) {
+          userGroups = (Set<String>) SecurityUtils.getSubject().getSession().getAttribute(SUBJECT_USER_GROUPS);
+        } else { // KnoxLdapRealm case
+          if (shiroSubject.getPrincipal() instanceof String) {
+            userGroups = new HashSet<>(shiroSubject.getPrincipals().asSet());
+            userGroups.remove(principal);
+          } else { // KnoxPamRealm case
+            Set<Principal> shiroPrincipals = new HashSet<>(
+                shiroSubject.getPrincipals().asSet());
+            userGroups = new HashSet<>(); // Here we are creating a new UserGroup
+            // so we don't need to remove a Principal
+            // In the case of LDAP the userGroup may have already Principal
+            // added to the list of groups, so it is not needed.
+            for (Principal shiroPrincipal : shiroPrincipals) {
+              userGroups.add(shiroPrincipal.toString());
+            }
+          }
         }
-      }
-      for (String userGroup : userGroups) {
-        Principal gp = new GroupPrincipal(userGroup);
-        principals.add(gp);
-      }
-      auditor.audit( Action.AUTHENTICATION , sourceUri, ResourceType.URI, ActionOutcome.SUCCESS, "Groups: " + userGroups );
+        for (String userGroup : userGroups) {
+          Principal gp = new GroupPrincipal(userGroup);
+          principals.add(gp);
+        }
+        auditor.audit(Action.AUTHENTICATION, sourceUri, ResourceType.URI,
+            ActionOutcome.SUCCESS, "Groups: " + userGroups);
 
-      // The newly constructed Sets check whether this Subject has been set read-only
-      // before permitting subsequent modifications. The newly created Sets also prevent
-      // illegal modifications by ensuring that callers have sufficient permissions.
-      //
-      // To modify the Principals Set, the caller must have AuthPermission("modifyPrincipals").
-      // To modify the public credential Set, the caller must have AuthPermission("modifyPublicCredentials").
-      // To modify the private credential Set, the caller must have AuthPermission("modifyPrivateCredentials").
-      javax.security.auth.Subject subject = new javax.security.auth.Subject(true, principals, Collections.emptySet(), Collections.emptySet());
-      javax.security.auth.Subject.doAs( subject, action );
+        // The newly constructed Sets check whether this Subject has been set read-only
+        // before permitting subsequent modifications. The newly created Sets also prevent
+        // illegal modifications by ensuring that callers have sufficient permissions.
+        //
+        // To modify the Principals Set, the caller must have AuthPermission("modifyPrincipals").
+        // To modify the public credential Set, the caller must have AuthPermission("modifyPublicCredentials").
+        // To modify the private credential Set, the caller must have AuthPermission("modifyPrivateCredentials").
+        javax.security.auth.Subject subject = new javax.security.auth.Subject(
+            true, principals, Collections.emptySet(), Collections.emptySet());
+        javax.security.auth.Subject.doAs(subject, action);
+      }
 
       return null;
     }
