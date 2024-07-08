c2bc57b8b6fd45f71a16e95bf85e995e44dbcf41
Sandeep Moré <moresandeep@gmail.com>
2024-07-07 20:14:25 -0400

    KNOX-3050 - fix * imports

--- gateway-service-auth/src/main/java/org/apache/knox/gateway/service/auth/PreAuthResource.java
+++ gateway-service-auth/src/main/java/org/apache/knox/gateway/service/auth/PreAuthResource.java
@@ -20,7 +20,12 @@ package org.apache.knox.gateway.service.auth;
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.*;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
