b3b0e4adc20215d0fc2ad1e79c74137debb97a99
hanicz <hanicz@users.noreply.github.com>
2024-09-10 20:35:55 +0200

    KNOX-3064: Set the private gatewayservices field to fix nullpointer exceptions (#935)

--- gateway-server/src/test/java/org/apache/knox/gateway/websockets/WebsocketMultipleConnectionTest.java
+++ gateway-server/src/test/java/org/apache/knox/gateway/websockets/WebsocketMultipleConnectionTest.java
@@ -27,6 +27,7 @@ import static org.apache.knox.gateway.config.GatewayConfig.DEFAULT_IDENTITY_KEY_
 import com.mycila.xmltool.XMLDoc;
 import com.mycila.xmltool.XMLTag;
 import org.apache.commons.io.FileUtils;
+import org.apache.knox.gateway.GatewayServer;
 import org.apache.knox.gateway.config.GatewayConfig;
 import org.apache.knox.gateway.config.impl.GatewayConfigImpl;
 import org.apache.knox.gateway.deploy.DeploymentFactory;
@@ -57,6 +58,7 @@ import javax.websocket.WebSocketContainer;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
@@ -249,7 +251,7 @@ public class WebsocketMultipleConnectionTest {
    * @param backend name of topology
    */
   private static void setupGatewayConfig(final String backend)
-      throws IOException {
+      throws Exception {
     services = new DefaultGatewayServices();
 
     URL serviceUrl = ClassLoader.getSystemResource("websocket-services");
@@ -380,6 +382,8 @@ public class WebsocketMultipleConnectionTest {
 
     EasyMock.replay(gatewayConfig);
 
+    setGatewayServices(services);
+
     try {
       services.init(gatewayConfig, options);
     } catch (ServiceLifecycleException e) {
@@ -391,6 +395,17 @@ public class WebsocketMultipleConnectionTest {
         .getService(ServiceType.TOPOLOGY_SERVICE);
     monitor.addTopologyChangeListener(topoListener);
     monitor.reloadTopologies();
+  }
+
+  /**
+   * Set the static GatewayServices field to the specified value.
+   *
+   * @param gws A GatewayServices object, or null.
+   */
+  private static void setGatewayServices(final GatewayServices gws) throws Exception {
+    Field gwsField = GatewayServer.class.getDeclaredField("services");
+    gwsField.setAccessible(true);
+    gwsField.set(null, gws);
   }
 
   private static File createDir() throws IOException {
