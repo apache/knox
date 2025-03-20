d68a04e691602f43bed876d959dede3d622a9a1e
thanicz <hanicz@users.noreply.github.com>
2025-03-20 19:50:40 +0100

    KNOX-3111: Integration tests for topology wide and global HSTS header configs

--- gateway-spi-common/src/main/java/org/apache/knox/gateway/GatewayTestConfig.java
+++ gateway-spi-common/src/main/java/org/apache/knox/gateway/GatewayTestConfig.java
@@ -1145,7 +1145,7 @@ public class GatewayTestConfig extends Configuration implements GatewayConfig {
 
   @Override
   public boolean isStrictTransportEnabled() {
-    return false;
+    return true;
   }
 
   @Override
