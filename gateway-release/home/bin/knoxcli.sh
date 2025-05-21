37d472b42d9c3d549ffbe7a9ad6ea9f60925d2f1
bonampak <14160522+bonampak@users.noreply.github.com>
2025-05-21 18:18:20 +0200

    KNOX-3153: Fix Java command invocations in knoxcli.sh

--- gateway-release/home/bin/knoxcli.sh
+++ gateway-release/home/bin/knoxcli.sh
@@ -63,7 +63,7 @@ function buildAppJavaOpts {
     fi
 
     # Add properties to enable Knox to run on JDK 17
-    JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    JAVA_VERSION=$("$JAVA" -version 2>&1 | awk -F '"' '/version/ {print $2}')
     CHECK_VERSION_17="17"
     if [[ "$JAVA_VERSION" == *"$CHECK_VERSION_17"* ]]; then
         echo "Java version is $CHECK_VERSION_17. Adding properties to enable Knox to run on JDK 17"
@@ -77,7 +77,7 @@ function main {
    setVerbose "$@"
    checkJava
    buildAppJavaOpts
-   $JAVA "${APP_JAVA_OPTS[@]}" -jar "$APP_JAR" "$@" || exit 1
+   "$JAVA" "${APP_JAVA_OPTS[@]}" -jar "$APP_JAR" "$@" || exit 1
    return 0
 }
 
