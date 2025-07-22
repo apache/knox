fefbff174de91737784daa1a11a305e7ee7fb700
Sandeep More <moresandeep@gmail.com>
2025-07-21 20:27:46 -0400

    save 1

--- gateway-topology-simple/src/main/java/org/apache/knox/gateway/topology/simple/XMLProviderConfiguration.java
+++ gateway-topology-simple/src/main/java/org/apache/knox/gateway/topology/simple/XMLProviderConfiguration.java
@@ -24,11 +24,11 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
