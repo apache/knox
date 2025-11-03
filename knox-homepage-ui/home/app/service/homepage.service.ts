95ddf59d62f1e0100636a808b5767c93b1ff5a72
thanicz <hanicz@users.noreply.github.com>
2025-11-03 13:25:59 +0100

    KNOX-3197: Updated corejs, swal2, rxjs, fixed the homepage service

--- knox-homepage-ui/home/app/service/homepage.service.ts
+++ knox-homepage-ui/home/app/service/homepage.service.ts
@@ -14,16 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import {ActivatedRoute} from '@angular/router';
-import Swal from 'sweetalert2';
-
-import 'rxjs/add/operator/toPromise';
-
-import {GeneralProxyInformation} from '../model/general.proxy.information';
-import {TopologyInformation} from '../model/topology.information';
-import {SessionInformation} from '../model/session.information';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2/dist/sweetalert2.esm.all.js';
+import { firstValueFrom } from 'rxjs';
+import { GeneralProxyInformation } from '../model/general.proxy.information';
+import { TopologyInformation } from '../model/topology.information';
+import { SessionInformation } from '../model/session.information';
 
 @Injectable()
 export class HomepageService {
@@ -35,88 +33,41 @@ export class HomepageService {
     publicCertUrl = this.apiUrl + 'publicCert?type=';
     topologiesUrl = this.apiUrl + 'topologies';
 
-    constructor(private http: HttpClient, private route: ActivatedRoute) {}
+    constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
     getGeneralProxyInformation(): Promise<GeneralProxyInformation> {
-        let headers = new HttpHeaders();
-        headers = this.addJsonHeaders(headers);
-        return this.http.get(this.generalProxyInformationUrl, { headers: headers})
-            .toPromise()
-            .then(response => response['generalProxyInfo'] as GeneralProxyInformation)
-            .catch((err: HttpErrorResponse) => {
-                console.debug('HomepageService --> getGeneralProxyInformation() --> '
-                               + this.generalProxyInformationUrl + '\n  error: ' + err.message);
-                if (err.status === 401) {
-                    window.location.assign(document.location.pathname);
-                } else {
-                    return this.handleError(err);
-                }
-            });
+        let headers = this.addJsonHeaders(new HttpHeaders());
+        return firstValueFrom(this.http.get(this.generalProxyInformationUrl, { headers }))
+            .then(resp => resp['generalProxyInfo'] as GeneralProxyInformation)
+            .catch(err => this.handleError(err));
     }
 
     getTopologies(): Promise<TopologyInformation[]> {
-        let headers = new HttpHeaders();
-        headers = this.addJsonHeaders(headers);
-        return this.http.get(this.topologiesUrl, { headers: headers})
-            .toPromise()
-            .then(response => response['topologyInformations'].topologyInformation as TopologyInformation[])
-            .catch((err: HttpErrorResponse) => {
-                console.debug('HomepageService --> getTopologies() --> ' + this.topologiesUrl + '\n  error: ' + err.message);
-                if (err.status === 401) {
-                    window.location.assign(document.location.pathname);
-                } else {
-                    return this.handleError(err);
-                }
-            });
+        let headers = this.addJsonHeaders(new HttpHeaders());
+        return firstValueFrom(this.http.get(this.topologiesUrl, { headers }))
+            .then(resp => resp['topologyInformations'].topologyInformation as TopologyInformation[])
+            .catch(err => this.handleError(err));
     }
 
     getSessionInformation(): Promise<SessionInformation> {
-        let headers = new HttpHeaders();
-        headers = this.addJsonHeaders(headers);
-        return this.http.get(this.sessionUrl, { headers: headers})
-            .toPromise()
-            .then(response => response['sessioninfo'] as SessionInformation)
-            .catch((err: HttpErrorResponse) => {
-                console.debug('HomepageService --> getSessionInformation() --> ' + this.sessionUrl + '\n  error: ' + err.message);
-                if (err.status === 401) {
-                    window.location.assign(document.location.pathname);
-                } else {
-                    return this.handleError(err);
-                }
-            });
+        let headers = this.addJsonHeaders(new HttpHeaders());
+        return firstValueFrom(this.http.get(this.sessionUrl, { headers }))
+            .then(resp => resp['sessioninfo'] as SessionInformation)
+            .catch(err => this.handleError(err,));
     }
 
-    logout(logoutUrl): Promise<JSON> {
-        let headers = new HttpHeaders();
-        headers = this.addJsonHeaders(headers);
-        return this.http.get(logoutUrl, { headers: headers})
-            .toPromise()
-            .then(response => response['loggedOut'])
-            .catch((err: HttpErrorResponse) => {
-                console.debug('HomepageService --> logout() --> ' + logoutUrl + '\n  error: ' + err.message);
-                if (err.status === 401) {
-                    window.location.assign(document.location.pathname);
-                } else {
-                    return this.handleError(err);
-                }
-            });
+    logout(logoutUrl: string): Promise<JSON> {
+        let headers = this.addJsonHeaders(new HttpHeaders());
+        return firstValueFrom(this.http.get(logoutUrl, { headers }))
+            .then(resp => resp['loggedOut'])
+            .catch(err => this.handleError(err));
     }
 
-    getProfile(profileName): Promise<JSON> {
-        let headers = new HttpHeaders();
-        headers = this.addJsonHeaders(headers);
-        return this.http.get(this.apiUrl + '/profiles/' + profileName, { headers: headers})
-            .toPromise()
-            .then(response => response)
-            .catch((err: HttpErrorResponse) => {
-                console.debug('HomepageService --> getProfile() --> ' + this.apiUrl + '/profiles/'
-                + profileName + '\n  error: ' + err.message);
-                if (err.status === 401) {
-                    window.location.assign(document.location.pathname);
-                } else {
-                    return this.handleError(err);
-                }
-            });
+    getProfile(profileName: string): Promise<JSON> {
+        let headers = this.addJsonHeaders(new HttpHeaders());
+        let url = `${this.apiUrl}/profiles/${profileName}`;
+        return firstValueFrom(this.http.get(url, { headers }))
+            .catch(err => this.handleError(err));
     }
 
     addJsonHeaders(headers: HttpHeaders): HttpHeaders {
@@ -132,13 +83,15 @@ export class HomepageService {
     }
 
     private handleError(error: HttpErrorResponse): Promise<any> {
-        Swal.fire({
+        void Swal.fire({
             icon: 'error',
             title: 'Oops!',
-            text: 'Something went wrong!\n' + (error.error ? error.error : error.statusText),
+            text: `Something went wrong!\n${error.error ? error.error : error.statusText}`,
             confirmButtonColor: '#7cd1f9'
         });
+
         return Promise.reject(error.message || error);
     }
+
 
 }
