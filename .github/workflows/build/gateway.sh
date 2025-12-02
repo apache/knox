ef1730b3702681f0c8f273751a81463dfbb047a3
Sandeep Moré <moresandeep@gmail.com>
2025-12-02 07:09:57 -0500

    Reformatted the test structure, added health test

--- .github/workflows/build/gateway.sh
+++ .github/workflows/build/gateway.sh
@@ -16,7 +16,7 @@
 
 # Move the KnoxShell directory to proper place
 # This is vecause of https://github.com/docker/compose/issues/4581#issuecomment-321386605
-mv /knox-runtime/knoxshell/* /knoxshell
+cp -r /knox-runtime/knoxshell/* /knoxshell
 
 # Start Knox
 java -jar /knox-runtime/bin/gateway.jar
