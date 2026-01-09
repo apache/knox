d4d1fccdd7620278e8b80264956e4689f8c485a8
Raghav Maheshwari <rmaheshwari@cloudera.com>
2026-01-09 20:44:51 +0530

    KNOX-3239: Fix for GatewayMessages error message typo

--- gateway-server/src/main/java/org/apache/knox/gateway/GatewayMessages.java
+++ gateway-server/src/main/java/org/apache/knox/gateway/GatewayMessages.java
@@ -720,7 +720,7 @@ public interface GatewayMessages {
   @Message(level = MessageLevel.INFO, text = "Using {0} implementation for {1}")
   void usingServiceImplementation(String implementation, String serviceType);
 
-  @Message(level = MessageLevel.ERROR, text = "Error while initiatalizing {0}: {1}")
+  @Message(level = MessageLevel.ERROR, text = "Error while initializing {0}: {1}")
   void errorInitializingService(String implementation, String error, @StackTrace(level = MessageLevel.DEBUG) Exception e);
 
   @Message(level = MessageLevel.ERROR,
